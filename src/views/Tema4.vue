381b17c1faba05a5e315722c6a1328dec26803c8
deividramirezarias <deivid.innovatec@gmail.com>
2023-04-05 17:45:13 -0500

    ajustes
    ajustes

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -59,7 +59,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/aC3uYxBaa8o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
 
     //p(data-aos="fade-down").mb-5 Para ver algunos ejemplos, te invitamos a descargar y revisar el 
       span.pur Anexo 1_ Ejemplos contratos.  
